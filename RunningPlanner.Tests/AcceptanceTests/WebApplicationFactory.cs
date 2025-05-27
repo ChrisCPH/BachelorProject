697a8c7e7316bef0d9c1255f2d705942b879411c
ChrisCPH <christoffer221199@hotmail.com>
2025-05-28 00:07:14 +0200

    Fixed a test should work with actions now

--- RunningPlanner.Tests/AcceptanceTests/WebApplicationFactory.cs
+++ RunningPlanner.Tests/AcceptanceTests/WebApplicationFactory.cs
@@ -4,19 +4,39 @@ using Microsoft.EntityFrameworkCore;
 using RunningPlanner.Data;
 using Microsoft.AspNetCore.Hosting;
 using MongoDB.Driver;
+using Mongo2Go;  // <-- Add this
 
 namespace RunningPlanner.Tests
 {
-    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>, IDisposable
     {
+        private MongoDbRunner? _mongoRunner;
+
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.ConfigureServices(services =>
             {
-                // Remove ALL existing DbContext configurations
+                // Start Mongo2Go server if not already started
+                if (_mongoRunner == null)
+                {
+                    _mongoRunner = MongoDbRunner.Start();
+                }
+
+                var mongoDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(IMongoClient));
+                if (mongoDescriptor != null)
+                {
+                    services.Remove(mongoDescriptor);
+                }
+
+                services.AddSingleton<IMongoClient>(sp =>
+                {
+                    return new MongoClient(_mongoRunner.ConnectionString);
+                });
+
                 var descriptors = services
                     .Where(d => d.ServiceType == typeof(DbContextOptions<RunningPlannerDbContext>) ||
-                              d.ServiceType == typeof(RunningPlannerDbContext))
+                                d.ServiceType == typeof(RunningPlannerDbContext))
                     .ToList();
 
                 foreach (var descriptor in descriptors)
@@ -24,12 +44,10 @@ namespace RunningPlanner.Tests
                     services.Remove(descriptor);
                 }
 
-                // Create a fresh service provider for InMemory
                 var serviceProvider = new ServiceCollection()
                     .AddEntityFrameworkInMemoryDatabase()
                     .BuildServiceProvider();
 
-                // Add DbContext with InMemory and isolated provider
                 services.AddDbContext<RunningPlannerDbContext>(options =>
                 {
                     options.UseInMemoryDatabase("TestDb");
@@ -38,14 +56,17 @@ namespace RunningPlanner.Tests
                     options.EnableSensitiveDataLogging();
                 });
 
-                // Build the provider and ensure database is created
                 var sp = services.BuildServiceProvider();
-                using (var scope = sp.CreateScope())
-                {
-                    var db = scope.ServiceProvider.GetRequiredService<RunningPlannerDbContext>();
-                    db.Database.EnsureCreated();
-                }
+                using var scope = sp.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<RunningPlannerDbContext>();
+                db.Database.EnsureCreated();
             });
+        }
+
+        public new void Dispose()
+        {
+            base.Dispose();
+            _mongoRunner?.Dispose();
         }
     }
 }
